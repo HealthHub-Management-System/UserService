48a3c9036cdbc63ad485d06d876917715099b006
Tosiek <antonizieciak@gmail.com>
2024-04-21 16:30:06 +0200

    get user by id test

--- backend/tests/api_test.go
+++ backend/tests/api_test.go
@@ -6,8 +6,10 @@ import (
 	mockDB "backend/utils/mock"
 	testUtil "backend/utils/test"
 	validatorUtil "backend/utils/validator"
+	"context"
 	"encoding/json"
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/go-chi/chi"
 	"github.com/google/uuid"
 	"net/http"
 	"net/http/httptest"
@@ -38,9 +40,8 @@ func TestGetUsers(t *testing.T) {
 	handler := http.HandlerFunc(usersAPI.List)
 
 	handler.ServeHTTP(rr, req)
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("Handler returned wrong status code. Expected: %d. Got: %d.", http.StatusOK, status)
-	}
+	status := rr.Code
+	testUtil.Equal(t, status, http.StatusOK)
 
 	var response users.ListResponse
 	err = json.NewDecoder(rr.Body).Decode(&response)
@@ -50,4 +51,45 @@ func TestGetUsers(t *testing.T) {
 	testUtil.Equal(t, len(responseUsers), 2)
 	testUtil.Equal(t, responseUsers[0].Name, "user1")
 	testUtil.Equal(t, responseUsers[1].Name, "user2")
+}
+
+func TestGetUser(t *testing.T) {
+	idString := "c50abe98-7f20-4cb9-b4a8-fbef37988e7f"
+	req, err := http.NewRequest("GET", "/api/v1/users/{id}", nil)
+	if err != nil {
+		t.Errorf("Error creating a new request: %v", err)
+	}
+
+	rctx := chi.NewRouteContext()
+	rctx.URLParams.Add("id", idString)
+	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, rctx))
+
+	l := logger.New(false)
+	v := validatorUtil.New()
+	db, mock, err := mockDB.NewMockDB()
+	testUtil.NoError(t, err)
+
+	id, err := uuid.Parse(idString)
+	testUtil.NoError(t, err)
+	usersAPI := users.New(l, db, v)
+	mockRows := sqlmock.NewRows([]string{"id", "name", "email"}).
+		AddRow(id, "user1", "email@email.com")
+
+	mock.ExpectQuery("^SELECT (.+) FROM \"users\" WHERE (.+)").
+		WithArgs(id, 1).
+		WillReturnRows(mockRows)
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(usersAPI.Read)
+
+	handler.ServeHTTP(rr, req)
+	status := rr.Code
+	testUtil.Equal(t, status, http.StatusOK)
+
+	var user users.User
+	err = json.NewDecoder(rr.Body).Decode(&user)
+	testUtil.NoError(t, err)
+
+	testUtil.Equal(t, user.Name, "user1")
+	testUtil.Equal(t, user.Email, "email@email.com")
 }
